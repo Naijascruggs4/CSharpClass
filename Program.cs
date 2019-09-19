48092caa921e39d307e79252e3ae123342dd8b1c
Naija Scruggs <Naijascruggs4@gmail.com>
2019-09-19 15:44:16 -0500

    Lesson 4 My Application Overview

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace CSharpClass
 {
+    /*This class is the start of my application 
+    Lesson 3 - My First Application */
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+        } // End Methos Main
+    } // End class
+} // end namespace
