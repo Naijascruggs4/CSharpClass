59e62892ff72c87af1cf466782959de301fad423
Naija Scruggs <Naijascruggs4@gmail.com>
2019-09-25 09:16:20 -0500

    Lesson 6 - Strings and More

--- Program.cs
+++ Program.cs
@@ -10,8 +10,26 @@ namespace CSharpClass
         static void Main(string[] args)
         {
            //Console.WriteLine("Hello World!");
-           MyLesson5Examples();
+           //MyLesson5Examples();
+           MyLesson6Examples();
         } // End Method Main
+
+        static void MyLesson6Examples()
+        {
+             Lesson6 mySix = new Lesson6();
+             mySix.MyTrimExample();
+             mySix.MyEqualsExample();
+             mySix.MyLowerUpperExample();
+             mySix.MyStringLength();
+             string myJoined = mySix.MyJoinedStrings("Happy", " Coding");
+             Console.WriteLine(myJoined);
+             string myFav = mySix.MyStringBuilder("tacos", 3);
+             Console.WriteLine(myFav);
+             mySix.MyExampleChar();
+             mySix.MyEscapeExample();
+             mySix.MyPlaceHolderExample("Injustice", "fight to the death game");
+                
+        } // end my lesson 6 examples
 
         static void MyLesson5Examples()
         {
